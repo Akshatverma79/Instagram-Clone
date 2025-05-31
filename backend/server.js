ea82214ece6b413b19ce1f12f56cec95d9effdc9
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-05-31 15:56:53 +0530

    backend connected and works on local host

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express"
 import dotenv from "dotenv";
 import connectDB from "./utls/connectDB.js";
 import login from "./roues/login-route.js";
+import cors from "cors";
 
 const app=express();
 
 dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 
 connectDB()
 
